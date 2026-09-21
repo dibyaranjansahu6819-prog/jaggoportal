ad1efb1599ae3c89c60b3e909821ef3abc797f30
Hitesh Kumar Satpathy <hiteshkumarr356@gmail.com>
2026-09-21 20:16:29 +0530

    Add AllowAny permission to Course and Subject views

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 from .models import Course, Subject
 from .serializers import CourseSerializer, SubjectSerializer
@@ -7,8 +8,10 @@ from .serializers import CourseSerializer, SubjectSerializer
 class CourseListView(generics.ListAPIView):
     queryset = Course.objects.all().order_by("name")
     serializer_class = CourseSerializer
+    permission_classes = [AllowAny]
 
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all().order_by("name")
     serializer_class = SubjectSerializer
+    permission_classes = [AllowAny]
